20c8fdf8b456b037a36a32469e5b2edc0d32606d
Jorge Alves <alvesdjorge@gmail.com>
2021-12-15 23:20:14 -0300

    add projetos modulos, funçoes e debug-error

---
+++ basecamp-javascript/modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Jorge', 36));
+
+console.log(mostraCidade('Jorge', 'São Bernardo do Campo'));
+
+console.log(mostraHobby('Jorge', 'Tocar Violão'));
